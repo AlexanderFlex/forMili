bc608d5808a5329a259abe58b83f48489bd2c3c5
Alexander Flex <ronion@yandex.ru>
2022-06-05 23:01:56 +0300

    miles calculation

---
+++ src/Mili.java
@@ -0,0 +1,12 @@
+public class Mili {
+    public static void main(String[] args) {
+        int price = 50_000;
+        int bonus = 20;
+
+        int mili;
+
+        mili = price / bonus;
+        System.out.println(mili);
+
+    }
+}
