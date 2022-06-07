c25cb6d087307dcb90d350989979fb53fcc89cfb
Alexander Flex <ronion@yandex.ru>
2022-06-07 23:05:43 +0300

    Mili Modernization

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int amount;
+
+        amount = cost / 20;
+
+        return amount;
+    }
+
 }
--- src/Mili.java
+++ src/Mili.java
@@ -1,12 +1,9 @@
 public class Mili {
     public static void main(String[] args) {
-        int price = 50_000;
-        int bonus = 20;
-
-        int mili;
-
-        mili = price / bonus;
-        System.out.println(mili);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
